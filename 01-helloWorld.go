f87de5a66e7e544f9e53cf0740bca79529faceb5
Andi Rustianto <anditodev46@gmail.com>
2025-03-11 10:42:02 +0700

    helloworld

--- 01-helloWorld.go
+++ 01-helloWorld.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+/*
+Untuk melakukan print / console.log ,
+kita perlu melakukan import module fmt terlebih dahulu
+*/
+
 func main() {
 	fmt.Println("Hello world")
 }
